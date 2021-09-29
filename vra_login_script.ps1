bcd29ab6bd733f5d15666416c452a567292068f6
Das <Siddartha.Das@FISGLOBAL.COM>
2021-09-29 14:34:24 +0530

    Signed-off-by: Siddartha Kumar Das(siddartha1192@gmail.com)

---
+++ vra_login_script.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module powervra -RequiredVersion 3.6.0
+# Enter your pass and stores it securely:
+$SecureString = Read-Host -AsSecureString 'Enter the Password' | ConvertFrom-SecureString | ConvertTo-SecureString
+# Users you password securly
+$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "<full_user_id>",$SecureString
+# Sets yous credentials to be used
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+$vra = Connect-vRAServer -Server c2c.cloud.kp.org -Credential (Get-Credential $MySecureCreds) -Tenant  KPPC -IgnoreCertRequirements
+write-host $vra
