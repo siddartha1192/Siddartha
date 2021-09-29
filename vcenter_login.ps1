a0d5246368eac42d4fa492d9c72c966c98ea12c1
Das <Siddartha.Das@FISGLOBAL.COM>
2021-09-29 14:41:35 +0530

    Signed-off-by: Siddartha Kumar Das(siddartha1192@gmail.com)

---
+++ vcenter_login.ps1
@@ -0,0 +1,8 @@
+﻿    $pass = '<password>'
+    $vCenters = @("full_vCenterName1","full_vCenterName2",full_vCenterName3)
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+foreach($vc in $vCenters)
+{
+Connect-VIServer $vc -User '<UserName>' -Password $pass 
+}
+
